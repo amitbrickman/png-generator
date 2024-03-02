def230fcf7d92e98da07b47d81c1cac9828ff863
amitbrickmanfa <amitbrickman@Amits-MacBook-Pro.local>
2024-03-02 14:31:07 +0200

    colors preset

--- netlify/functions/puppet.js
+++ netlify/functions/puppet.js
@@ -9,6 +9,13 @@ export async function handler(event, context) {
   try {
     const { days = 0, textColor = 'white' } = event.queryStringParameters;
 
+    const colors = {
+      gold: `radial-gradient(ellipse farthest-corner at right bottom, #FEDB37 0%, #FDB931 8%, #9f7928 30%, #8A6E2F 40%, transparent 80%),
+      radial-gradient(ellipse farthest-corner at left top, #FFFFFF 0%, #FFFFAC 8%, #D1B464 25%, #5d4a1f 62.5%, #5d4a1f 100%)`,
+    }
+
+    const selectedColor = colors[textColor] || textColor;
+
     const image = await nodeHtmlToImage({
       html: `
       <html>
@@ -29,7 +36,7 @@ export async function handler(event, context) {
             }
 
             div {
-                background: url("https://ios-widgets.vercel.app/static/images/tiger_pattern.png"), ${textColor};
+                background: url("https://ios-widgets.vercel.app/static/images/tiger_pattern.png"), ${selectedColor};
                 background-repeat: no-repeat;
                 background-size: cover;
                 -webkit-background-clip: text;
